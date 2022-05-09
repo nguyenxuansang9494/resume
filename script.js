578ee38e89ffe3ba5b8e49830655256a322beb92
Sang Nguyen <nguyenxuansang9494@gmail.com>
2022-05-09 23:41:15 +0700

    add getCommand and parse function

--- script.js
+++ script.js
@@ -11,6 +11,26 @@ document.addEventListener("keydown", (e) => {
     a.innerHTML = input.value;
     a.style.display = "block";
     executedCommand.append(a);
+    getCommand(parse(input.value))();
     input.value = label;
   }
 });
+
+const parse = (aString) => {
+  return aString.split(label)[1];
+};
+
+const getCommand = (command) => {
+  if (command == "clear") {
+    return () => {
+      executedCommand.innerHTML = "";
+    };
+  } else {
+    return () => {
+      let a = document.createElement("a");
+      a.innerHTML = "invalid command.";
+      a.style.display = "block";
+      executedCommand.append(a);
+    };
+  }
+};
