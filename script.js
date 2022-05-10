ccc1a8c015ba0772594b8e0550ec9880efcd0481
sang nguuyen <nguyenxuansang9494@gmail.com>
2022-05-10 13:04:27 +0700

    refractor code

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-const input = document.getElementById("input");
-const inputGroup = document.getElementById("input-group");
-const executedCommand = document.getElementById("executed-commands");
-const label = "recruiter@sangnguyenresume [~] $ ";
+const labelString =
+  "<span style='font-weight: bold;'>recruiter@sangnguyenresume [~] $ </span>";
 let workingDirectory = "/home/resume";
+const playground = document.createElement("div");
+playground.id = "playground";
+const executedCommands = document.createElement("div");
+executedCommands.id = "executed-commands";
+executedCommands.className = "text-format";
+const inputGroup = document.createElement("div");
+inputGroup.id = "input-group";
+const label = document.createElement("p");
+label.id = "label";
+label.className = "text-format";
+label.innerHTML = labelString;
+const input = document.createElement("div");
+input.id = "input";
+input.contentEditable = true;
+input.className = "text-format";
 
-const displayResult = (content) => {
+window.addEventListener("load", () => {
+  const app = document.getElementById("app");
+  inputGroup.append(label,input);
+  playground.append(executedCommands, inputGroup);
+  app.append(playground);
+});
+
+const displayResult = (content, style = {}) => {
   let p = document.createElement("p");
   p.innerHTML = content;
   p.style.display = "block";
   p.style.marginBottom = "2px";
   p.style.marginTop = "0px";
-  executedCommand.append(p);
+  executedCommands.append(p);
 };
 
 const getCommand = (command) => {
   switch (command) {
     case "clear":
       return () => {
-        executedCommand.innerHTML = "";
+        executedCommands.innerHTML = "";
       };
     case "help":
       return () => {
@@ -43,8 +63,8 @@ document.addEventListener("click", () => {
 input.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
     e.preventDefault();
-    displayResult(`${label + " " + input.value}`);
-    getCommand(input.value)();
-    input.value = "";
+    displayResult(`${labelString + " " + input.innerText}`);
+    getCommand(input.innerText)();
+    input.innerText = "";
   }
 });
