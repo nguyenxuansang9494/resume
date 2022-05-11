a1f03ec561dd2e0556ed8a0f871d1dbace0066b1
Sang Nguyen <64133120+nguyenxuansang9494@users.noreply.github.com>
2022-05-11 13:38:53 +0700

    Update showmeyourcode.js

--- src/modules/command/cmd/showmeyourcode.js
+++ src/modules/command/cmd/showmeyourcode.js
@@ -3,7 +3,7 @@ import { displayResult, executedCommands } from "../../ui/render.js";
 export const showmeyourcode = () => {
   displayResult(
     executedCommands,
-    "It's very happy to know that you want to read my code!!"
+    "I'm very happy to know that you want to read my code!!"
   );
   window.open("https://github.com/nguyenxuansang9494/resume");
 };
