f64343a6c1804158a53a1a996eec33b302b496e0
Sang Nguyen <nguyenxuansang9494@gmail.com>
2022-05-13 03:48:24 +0700

    fix node

--- src/modules/system/filesystem/node.js
+++ src/modules/system/filesystem/node.js
@@ -2,6 +2,7 @@ export function Node(isFile, name, parent, children, fileContent) {
   this.isFile = isFile;
   this.name = name;
   this.children = children;
+  this.parent = parent
   this.fileContent = fileContent;
   if (isFile) {
     this.children = undefined;
